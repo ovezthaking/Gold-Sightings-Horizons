bede88fb3f5ef663668d9dd8211fbdc8d7a51d33
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-06 16:01:00 +0100

    searching data by query params

--- Wild Horizons/server.js
+++ Wild Horizons/server.js
@@ -2,15 +2,23 @@ import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
 import sendJSONResponse from './utils/sendJSONResponse.js'
 import getDataByPathParams from './utils/getDataByPathParams.js'
+import getDataByQueryParams from './utils/getDataByQueryParams.js'
 
 const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
+
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+
+    const queryObj = Object.fromEntries(urlObj.searchParams)
     
-    if (req.url === '/api') {
+    
+    if (urlObj.pathname === '/api') {
         if (req.method === 'GET') {
-            sendJSONResponse(res, 200, destinations)
+            let filteredData = getDataByQueryParams(destinations, queryObj)
+
+            sendJSONResponse(res, 200, filteredData)
         }
     }
     else if (req.url.startsWith('/api/continent')) {
---
+++ Wild Horizons/utils/getDataByQueryParams.js
@@ -0,0 +1,22 @@
+const getDataByQueryParams = (data, queryObj) => {
+    const { country, continent, is_open_to_public } = queryObj
+
+    if (country) {
+        data = data.filter(destination => 
+            destination['country'].toLowerCase() === country.toLowerCase())
+    }
+    
+    if (continent) {
+        data = data.filter(destination => 
+            destination['continent'].toLowerCase() === continent.toLowerCase())
+    }
+
+    if(is_open_to_public) {
+        data = data.filter(destination => 
+            destination['is_open_to_public'] === JSON.parse(is_open_to_public))
+    }
+
+    return data
+}
+
+export default getDataByQueryParams
