d38a450008760f17131869f44ef047bb643bbdfe
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-06 16:02:15 +0100

    to lower case before JSON parse

--- Wild Horizons/utils/getDataByQueryParams.js
+++ Wild Horizons/utils/getDataByQueryParams.js
@@ -5,7 +5,7 @@ const getDataByQueryParams = (data, queryObj) => {
         data = data.filter(destination => 
             destination['country'].toLowerCase() === country.toLowerCase())
     }
-    
+
     if (continent) {
         data = data.filter(destination => 
             destination['continent'].toLowerCase() === continent.toLowerCase())
@@ -13,7 +13,7 @@ const getDataByQueryParams = (data, queryObj) => {
 
     if(is_open_to_public) {
         data = data.filter(destination => 
-            destination['is_open_to_public'] === JSON.parse(is_open_to_public))
+            destination['is_open_to_public'] === JSON.parse(is_open_to_public.toLowerCase()))
     }
 
     return data
