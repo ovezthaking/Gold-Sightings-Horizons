13c32356fe88a60017fb91e379c947091c9c505d
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-06 04:48:22 +0100

    Set up the Wild Horizons API project and handle first data in happy path

---
+++ Wild Horizons/server.js
@@ -0,0 +1,19 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+
+const PORT = 8000
+
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+    
+    if (req.url === '/api') {
+        if (req.method === 'GET') {
+            res.setHeader('Content-Type', 'application/json')
+            res.statusCode = 200
+            res.end(JSON.stringify(destinations))
+        }
+    }
+    
+})
+
+server.listen(PORT, () => console.log(`server running on post ${PORT}`))
