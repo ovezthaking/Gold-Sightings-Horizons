be56dc8cc3f24815c513faa566d258cbb410ee05
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-06 16:07:41 +0100

    Add CORS and rename project folder

--- Wild Horizons API/utils/sendJSONResponse.js
+++ Wild Horizons API/utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 const sendJSONResponse = (res, statusCode, payload) => {
     res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
     res.statusCode = statusCode
     res.end(JSON.stringify(payload))
 }
