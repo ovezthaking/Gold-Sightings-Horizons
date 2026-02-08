95e0a467b64308552c46cceb2a896c358c8a794c
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-08 23:53:34 +0100

    Sanitize user's input - no tricky malicious code

--- From The Other Side/handlers/routeHandlers.js
+++ From The Other Side/handlers/routeHandlers.js
@@ -1,6 +1,7 @@
 import addNewSighting from "../utils/addNewSighting.js"
 import getData from "../utils/getData.js"
 import parseJSONBody from "../utils/parseJSONBody.js"
+import sanitizeInput from "../utils/sanitizeInput.js"
 import sendResponse from "../utils/sendResponse.js"
 
 export const handleGet = async (res) => {
@@ -18,7 +19,9 @@ export const handlePost = async (req, res) => {
 
     try {
         const parsedBody = await parseJSONBody(req)
-        await addNewSighting(parsedBody)
+        const sanitizedBody = sanitizeInput(parsedBody)
+        
+        await addNewSighting(sanitizedBody)
         sendResponse(res, 201, 'application/json', JSON.stringify(parsedBody))
     } catch (err) {
         sendResponse(res, 400, 'application/json', JSON.stringify({error: err}))
---
+++ From The Other Side/utils/sanitizeInput.js
@@ -0,0 +1,19 @@
+import sanitizeHtml from 'sanitize-html'
+
+export default function sanitizeInput(data) {
+    const sanitizedData = {}
+
+    for (const [key, value] of Object.entries(data)) {
+        if (typeof value === 'string') {
+            sanitizedData[key] = sanitizeHtml(
+                value,
+                {allowedTags: ['b'], allowedAttributes: {}}
+            )
+        }
+        else {
+            sanitizedData[key] = value
+        }
+    }
+
+    return sanitizedData
+}
