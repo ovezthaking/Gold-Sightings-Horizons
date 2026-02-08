705b3b2b16091801ad4f13edf965a43ea77b9de4
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-08 18:46:12 +0100

    Serve all static files

--- From The Other Side/utils/serveStatic.js
+++ From The Other Side/utils/serveStatic.js
@@ -1,14 +1,23 @@
 import path from "node:path"
 import fs from 'node:fs/promises'
 import sendResponse from "./sendResponse.js"
+import getContentType from "./getContentType.js"
 
 const serveStatic = async (req, res, baseDir) => {
-    const filePath = path.join(baseDir, 'public', 'index.html')
+    const publicDir = path.join(baseDir, 'public')
+    const filePath = path.join(
+        publicDir,
+        req.url === '/' ? 'index.html' : req.url
+    )
     
     try {
         const content = await fs.readFile(filePath)
 
-        sendResponse(res, 200, 'text/html', content)
+        const ext = path.extname(filePath)
+
+        const contentType = getContentType(ext)
+
+        sendResponse(res, 200, contentType, content)
     } catch (err) {
         console.error('Error', err)
     }
