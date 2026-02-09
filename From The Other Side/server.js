9e8dc7b805d36ee5ea3cd5bbf49042c3859f0d40
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-09 03:59:22 +0100

    improve error handling on endpoints

--- From The Other Side/server.js
+++ From The Other Side/server.js
@@ -16,9 +16,19 @@ const server = http.createServer(async (req, res) => {
         else if (req.method === 'POST') {
             return await handlePost(req, res)
         }
+        else {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify({status: res.statusCode, error: 'Method not allowed'}))
+        }
     }
     else if (req.url === '/api/news') {
         return await handleNews(req, res)
+    }
+    else if (req.url.startsWith('/api')) {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({status: res.statusCode, error: 'Not found'}))
     }
     else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
