31150cfeee11b54aa1dd00c15877013da2ed33d9
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-09 16:10:05 +0100

    Function to instant generating prices on backend

---
+++ Gold Digger/handlers/routeHandlers.js
@@ -0,0 +1,21 @@
+export const handlePrice = async (req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/event-stream')
+    res.setHeader('Cache-Control', 'no-cache')
+    res.setHeader('Connection', 'keep-alive')
+
+    const basePrice = 3694
+
+    setInterval(() => {
+        let randomDecimals = Math.round((Math.random()-0.5)*80)/100
+
+        let finalPrice = basePrice + randomDecimals
+
+        res.write(
+            `data: ${JSON.stringify({
+                event: 'price-update',
+                price: finalPrice
+            })}\n\n`
+        )
+    },2000)
+}
--- Gold Digger/server.js
+++ Gold Digger/server.js
@@ -1,13 +1,17 @@
 import http from 'node:http'
 import serveStatic from './utils/serveStatic.js'
+import { handlePrice } from './handlers/routeHandlers.js'
 
 
 const PORT = 8000
 const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     if (req.url === '/api'){
         return
+    }
+    else if (req.url === '/api/price') {
+        return await handlePrice(req, res)
     }
     else if (!req.url.startsWith('/api')) {
         serveStatic(req, res, __dirname)
