5094c843f922d429da491effa948a49cf08eda9e
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-10 01:20:00 +0100

    refactor to use sendResponse function

--- From The Other Side/server.js
+++ From The Other Side/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import serveStatic from './utils/serveStatic.js'
 import { handleGet, handleNews, handlePost } from './handlers/routeHandlers.js'
+import sendResponse from './utils/sendResponse.js'
 
 const PORT = 8000
 
@@ -17,18 +18,16 @@ const server = http.createServer(async (req, res) => {
             return await handlePost(req, res)
         }
         else {
-            res.statusCode = 404
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify({status: res.statusCode, error: 'Method not allowed'}))
+            sendResponse(res, 404, 'application/json', JSON.stringify(
+                {status: res.statusCode, error: 'Method not allowed'}))
         }
     }
     else if (req.url === '/api/news') {
         return await handleNews(req, res)
     }
     else if (req.url.startsWith('/api')) {
-        res.statusCode = 404
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({status: res.statusCode, error: 'Not found'}))
+        sendResponse(res, 404, 'application/json', JSON.stringify(
+                {status: res.statusCode, error: 'Not found'}))
     }
     else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
