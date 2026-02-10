654f03ec2df4e96a20389d5e222b6ce4746e10a7
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-10 01:21:45 +0100

    Error handling for routes

--- Gold Digger/server.js
+++ Gold Digger/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import serveStatic from './utils/serveStatic.js'
 import { handlePost, handlePrice } from './handlers/routeHandlers.js'
+import sendResponse from './utils/sendResponse.js'
 
 
 const PORT = 8000
@@ -11,9 +12,18 @@ const server = http.createServer(async (req, res) => {
         if (req.method === 'POST') {
             return await handlePost(req, res)
         }
+        else {
+            sendResponse(res, 404, 'application/json', JSON.stringify(
+                {error: 'Method not allowed'}
+            ))
+        }
     }
     else if (req.url === '/api/price') {
         return await handlePrice(req, res)
+    }
+    else if (req.url.startsWith('/api')) {
+        sendResponse(res, 404, 'application/json', JSON.stringify(
+                {status: res.statusCode, error: 'Not found'}))
     }
     else if (!req.url.startsWith('/api')) {
         serveStatic(req, res, __dirname)
